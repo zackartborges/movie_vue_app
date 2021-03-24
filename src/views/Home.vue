ab8e4a6ccd429226005c54083b1be63c27382250
Zack Borges <zackartborges@gmail.com>
2021-03-24 18:48:46 -0400

    adds index view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="movie in movies" v-bind:key="movie">
+      <h1>{{ movie.title }}</h1>
+    </div>
   </div>
 </template>
 <style></style>
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome tweeo Vue.js!",
+      movies: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/api/movies").then((response) => {
+        this.movies = response.data;
+        console.log("all movies:", this.movies);
+      });
+    },
+  },
 };
 </script>
