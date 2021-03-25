a5498e4e0a5f2410c6868e46c8c71ee58dab1921
Zack Borges <zackartborges@gmail.com>
2021-03-25 18:37:41 -0400

    adds create function. new movies are n database, but not showing up in web page?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieTitle" />
+      Year:
+      <input type="text" v-model="newMovieYear" />
+      Plot:
+      <input type="text" v-model="newMoviePlot" />
+      Director:
+      <input type="text" v-model="newMovieDirector" />
+      <button v-on:click="createMovie">Add a Movie!</button>
+    </div>
     <div v-for="movie in movies" v-bind:key="movie">
       <h1>{{ movie.title }}</h1>
     </div>
@@ -14,6 +25,11 @@ export default {
     return {
       message: "Welcome tweeo Vue.js!",
       movies: [],
+      // currentMovie: {},
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+      newMovieDirector: "",
     };
   },
   created: function () {
@@ -24,6 +40,19 @@ export default {
       axios.get("/api/movies").then((response) => {
         this.movies = response.data;
         console.log("all movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      console.log("adding movie..");
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector,
+      };
+      axios.post("/api/movies", params).then((response) => {
+        console.log("Successfully added movie!", response.data);
+        this.movies.push(response.data);
       });
     },
   },
