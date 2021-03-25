727be89d86794c32cdfc2191a6c3ae1c26367076
Zack Borges <zackartborges@gmail.com>
2021-03-25 19:28:46 -0400

    adds crud - update destroy create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,18 +14,44 @@
     </div>
     <div v-for="movie in movies" v-bind:key="movie">
       <h1>{{ movie.title }}</h1>
+      <button v-on:click="showMovie(movie)">More info!</button>
     </div>
+    <dialog id="movie-details">
+      <form method="dialog">
+        <h1>Movie Info</h1>
+        <p>
+          Title:
+          <input type="text" v-model="currentMovie.title" />
+        </p>
+        <p>
+          Year:
+          <input type="text" v-model="currentMovie.year" />
+        </p>
+        <p>
+          Plot:
+          <input type="text" v-model="currentMovie.plot" />
+        </p>
+        <p>
+          Director:
+          <input type="text" v-model="currentMovie.director" />
+        </p>
+        <button v-on:click="updateMovie(currentMovie)">Update Movie Info</button>
+        <button v-on:click="destroyMovie(currentMovie)">Terminate Movie</button>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 <style></style>
 <script>
 import axios from "axios";
+// import func from "vue-editor-bridge";
 export default {
   data: function () {
     return {
       message: "Welcome tweeo Vue.js!",
       movies: [],
-      // currentMovie: {},
+      currentMovie: {},
       newMovieTitle: "",
       newMovieYear: "",
       newMoviePlot: "",
@@ -53,6 +79,29 @@ export default {
       axios.post("/api/movies", params).then((response) => {
         console.log("Successfully added movie!", response.data);
         this.movies.push(response.data);
+      });
+    },
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
+    destroyMovie: function (movie) {
+      axios.delete("/api/movies/" + movie.id).then((response) => {
+        console.log("Success!", response.data);
+        var index = this.movies.indexOf(movie);
+        this.movies.splice(index, 1);
+      });
+    },
+    updateMovie: function (movie) {
+      var params = {
+        title: movie.title,
+        year: movie.year,
+        plot: movie.plot,
+        director: movie.director,
+      };
+      axios.patch("/api/recipes/" + movie.id, params).then((response) => {
+        console.log("Succeeeesss!!", response.data);
       });
     },
   },
